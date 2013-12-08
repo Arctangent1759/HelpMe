dc6651b5c233acbd5e6e51b44716eff871d89e9f
nitin <nitin.sadras@gmail.com>
2013-12-08 00:25:59 -0800

    response class that sends arbitrary JSON objects to clients via node-gcm

---
+++ Server/response.js
@@ -0,0 +1,19 @@
+var gcm = require('node-gcm');
+function Response(){
+    this.gcmKey = 'AIzaSyA2s54h6S-taxs3Rm8wvm6fIW74wJOQqQ4'; // API Key
+    
+    this.write = function(msgData, regId){
+        var message = new gcm.Message({
+            collapseKey: 'Pending Help Requests',
+            data: msgData
+        });
+        var sender = new gcm.Sender(this.gcmKey);
+        var regIds = [regId];
+        sender.send(message, regIds, 4, function(err, result){
+            if(err){
+                console.log("Error sending message to client");
+            }
+        }
+    }
+     
+}
