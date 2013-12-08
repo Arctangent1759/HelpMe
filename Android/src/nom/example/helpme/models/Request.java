d1a30488fcdfa228806762b3a86d6297582fce5e
Austin Chen <akrolsmir@gmail.com>
2013-12-08 11:13:05 -0800

    Dump of Android code

--- Android/src/nom/example/helpme/models/Request.java
+++ Android/src/nom/example/helpme/models/Request.java
@@ -6,73 +6,52 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 //EAch request is request made by person. Eventually saved in model. 
 public class Request {
+	public String username, title, desc, _id;
+	public boolean active, helpComing, urgent;
+	public Loc loc, epicenter;
 
-	public double lat;
-	public double lng;
-	public String username;
-	public boolean urgent;
-	public boolean mine;
-	public String title;
-	public String description;
-	public static final int TRUNCATED_TITLE_LENGTH = 14;
-	public static final int TRUNCATED_DESCRIP_LENGTH = 20;
-	
-	//TODO write a constructor
-	public Request(double lat, double lng, String username, boolean urgent, boolean mine, String title, String description)
-	{
-		this.lat = lat;
-		this.lng = lng;
-		this.username = username;
-		this.urgent = urgent;
-		this.mine = mine;
-		this.title = title;
-		this.description = description;	
-	}
+	private final static int TRUNCATED_DESCRIP_LENGTH = 20;
 
-	public MarkerOptions getMarker() 
-	{
+	public MarkerOptions getMarker() {
 		String truncateTitle = null;
 		String truncateDescription = null;
 		int leftOverLength;
 		String overflowedString = "";
 		//Truncate the title and Description if too long. 
 		if (title.length() >= 15)
-			truncateTitle = title.substring(0, TRUNCATED_TITLE_LENGTH) + "...";
+			truncateTitle = title.substring(0, 20) + "...";
 		else
 			truncateTitle = title;
-		
-		
-		if (description.length() >= 20)
-			truncateDescription = description.substring(0, TRUNCATED_DESCRIP_LENGTH) + "..." + " --" + username;
-		else
-		{
-			leftOverLength = 20-description.length();
-			while (leftOverLength > 0)
-			{
+
+		if (desc.length() >= 20)
+			truncateDescription = desc.substring(0, TRUNCATED_DESCRIP_LENGTH) + "..." + " --"
+					+ username;
+		else {
+			leftOverLength = 20 - desc.length();
+			while (leftOverLength > 0) {
 				overflowedString = overflowedString + " ";
 				leftOverLength--;
 			}
-			truncateDescription = description + overflowedString + "    --" + username;
+			truncateDescription = desc + overflowedString + "    --" + username;
 		}
-		
+
 		MarkerOptions marker = new MarkerOptions();
 		marker.title(truncateTitle);
 		marker.snippet(truncateDescription); //TODO truncate?
-		marker.position(new LatLng(lat, lng));
-		
+		marker.position(new LatLng(loc.y, loc.x));
+
 		//Sets color.
-		if (mine == true)
-		{
+		if (username == null) {//TODO fix
 			marker.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
-		} 
-		else if (urgent == true)
-		{
+		} else if (urgent == true) {
 			marker.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
-		} 
-		else
-		{
+		} else {
 			marker.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW));
 		}
 		return marker;
+	}
+
+	public class Loc {
+		public double x, y;
 	}
 }
