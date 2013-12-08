2999667c42833b48a072b5a2cad95959ec62c299
nitin <nitin.sadras@gmail.com>
2013-12-08 07:33:50 -0800

    added response interface support for sending GCM messages to multiple users

--- Server/response.js
+++ Server/response.js
@@ -2,13 +2,16 @@ var gcm = require('node-gcm');
 function Response(){
     this.gcmKey = 'AIzaSyCsXUPS7fPyGt5TD-hxTCVXjU8UvHyqmcU'; // API Key
     
-    this.write = function(msgData, regId){
+    /**
+     * msgData - JSON object to be sent to GCM
+     * regIds - list of one or more GCM regids
+    **/
+    this.write = function(msgData, regIds){
         var message = new gcm.Message({
             //collapseKey: 'Pending Help Requests',
             data: msgData
         });
         var sender = new gcm.Sender(this.gcmKey);
-        var regIds = [regId];
         sender.send(message, regIds, 4, function(err, result){
             if(err){
                 console.log("Error sending message to client");
